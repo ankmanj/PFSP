2ce1323196ce54bad2c4ac4bbe06990e790d7429
Ankith Manjunath <barneyhomeautomation@gmail.com>
2024-03-25 14:22:05 +0100

    Add pfsp_core cleanup in simple_run.sh

--- simple_run.sh
+++ simple_run.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 current_Path=$(pwd)
-
 build_path="build"
-cd $current_Path/pfsp_core/$build_path
-make
+
+pfsp_build="$current_Path/build"
+psfp_core_build="$current_Path/pfsp_core/$build_path"
+
+
+
+
+# Check if build directory exists
+if [ ! -d "$psfp_core_build" ]; then
+    echo "Build directory doesn't exist. Creating it..."
+    mkdir -p "$psfp_core_build"    
+else
+    echo "Build directory already exists."
+fi
+cd $psfp_core_build
+echo $(pwd)
+echo "Clean pfsp_core build directory"
+rm -rf ./*
+
+echo "Building PFSP platform"
+cmake .. && make
+
 
 cd $current_Path/pfsp_core/$build_path/pfsp_component_plugin/error_handler
 cp -rf libPFSP_plugin_error_handler.so $current_Path/pfsp_core/pfsp_lib/libs
